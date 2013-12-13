ada1c20726d3a6b11b8990924327e1f9fcbc00cf
RedmenNL <iDynious@gmail.com>
2013-12-13 13:14:48 +0100

    commit from 12/13/13 13:14:48. 0 deletions, 1 adds, 1 modifications

---
+++ java/com/dynious/blex/BlockExtenders.java
@@ -0,0 +1,16 @@
+package com.dynious.blex;
+
+import com.dynious.blex.lib.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid = Reference.modid, name = Reference.name, version = Reference.version)
+public class BlockExtenders
+{
+    @Mod.Instance(Reference.modid)
+    public static BlockExtenders instance;
+
+    @SidedProxy(clientSide = )
+
+}
---
+++ java/com/dynious/blex/lib/Reference.java
@@ -0,0 +1,9 @@
+package com.dynious.blex.lib;
+
+public class Reference
+{
+    public static final String modid = "BlEx";
+    public static final String name = "Block Extenders";
+    public static final String version = "0.1";
+    public static final String clientProxy = "com.dynious.blex.proxy.ClientProxy";
+}
