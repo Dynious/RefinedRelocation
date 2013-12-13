3ec68487186477d9f912f159d5d84f8febccb3b2
RedmenNL <iDynious@gmail.com>
2013-12-13 13:24:14 +0100

    commit from 12/13/13 13:24:14. 0 deletions, 0 adds, 1 modifications

--- java/com/dynious/blex/block/ModBlocks.java
+++ java/com/dynious/blex/block/ModBlocks.java
@@ -1,9 +1,11 @@
 package com.dynious.blex.block;
 
+import cpw.mods.fml.common.registry.GameRegistry;
+
 public class ModBlocks
 {
     public static void init()
     {
-        
+        GameRegistry.registerBlock();
     }
 }
