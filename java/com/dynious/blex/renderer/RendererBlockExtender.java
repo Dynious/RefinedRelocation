bbdc137725160cdb4738fb74afcead6603629d4a
RedmenNL <iDynious@gmail.com>
2013-12-14 20:24:13 +0100

    commit from 12/14/13 20:24:13. 0 deletions, 0 adds, 1 modifications

--- java/com/dynious/blex/renderer/RendererBlockExtender.java
+++ java/com/dynious/blex/renderer/RendererBlockExtender.java
@@ -6,6 +6,7 @@ import com.dynious.blex.tileentity.TileBlockExtender;
 import cpw.mods.fml.client.FMLClientHandler;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.common.ForgeDirection;
 import org.lwjgl.opengl.GL11;
 
 public class RendererBlockExtender extends TileEntitySpecialRenderer
@@ -26,6 +27,8 @@ public class RendererBlockExtender extends TileEntitySpecialRenderer
 
             FMLClientHandler.instance().getClient().renderEngine.bindTexture(Resources.MODEL_TEXTURE_ADVANCED_FILTERED_BLOCK_EXTENDER);
 
+            rotate(tile);
+
             GL11.glTranslated(x + 0.5F, y, z + 0.5F);
             GL11.glScalef(0.5F, 0.5F, 0.5F);
 
@@ -35,6 +38,30 @@ public class RendererBlockExtender extends TileEntitySpecialRenderer
 
             GL11.glEnable(GL11.GL_LIGHTING);
             GL11.glEnable(GL11.GL_CULL_FACE);
+        }
+    }
+
+    public void rotate(TileBlockExtender tile)
+    {
+        switch(tile.getConnectedDirection())
+        {
+            case DOWN:
+                break;
+            case UP:
+                GL11.glRotatef(180F, 1F, 0F, 0F);
+                break;
+            case NORTH:
+                GL11.glRotatef(-90F, 1F, 0F, 0F);
+                break;
+            case SOUTH:
+                GL11.glRotatef(90F, 1F, 0F, 0F);
+                break;
+            case WEST:
+                GL11.glRotatef(-90F, 0F, 0F, 1F);
+                break;
+            case EAST:
+                GL11.glRotatef(90F, 0F, 0F, 1F);
+                break;
         }
     }
 }
