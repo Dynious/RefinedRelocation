41a04bfcdad19fef20eefb77d953bb961297a3f9
RedmenNL <iDynious@gmail.com>
2013-12-14 09:54:14 +0100

    STARTING DEVELOPING AGAIN!

--- java/com/dynious/blex/tileentity/TileAdvancedBlockExtender.java
+++ java/com/dynious/blex/tileentity/TileAdvancedBlockExtender.java
@@ -25,6 +25,7 @@ public class TileAdvancedBlockExtender extends TileBlockExtender
                 {
                     bestSlot = slot;
                     bestSize = stack.stackSize;
+                    //STARTING AGAIN
                 }
             }
         }
