e869f77fc472570474663f000c2ecc0cf4269a1d
squeek <squeek502@hotmail.com>
2014-02-08 01:03:51 -0800

    Improved BlockHelper getDisplayName functions by using similar methods to Waila

--- java/com/dynious/blex/helper/BlockHelper.java
+++ java/com/dynious/blex/helper/BlockHelper.java
@@ -1,26 +1,75 @@
 package com.dynious.blex.helper;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
 public class BlockHelper
 {
+    public static final String nullBlockString = "<NONE>";
+
     public static String getBlockDisplayName(World world, int x, int y, int z)
     {
-        return getTileEntityDisplayName(world.getBlockTileEntity(x, y, z));
+        int blockID = world.getBlockId(x, y, z);
+        Block block = Block.blocksList[blockID];
+        if (block != null)
+        {
+            ItemStack pickedItemStack = block.getPickBlock(null, world, x, y, z);
+            if (pickedItemStack != null)
+                return getItemStackDisplayName(pickedItemStack);
+
+            List<ItemStack> dropped = block.getBlockDropped(world, x, y, z, world.getBlockMetadata(x, y, z), 0);
+            if (!dropped.isEmpty())
+            {
+                Collections.sort(dropped, new Comparator<ItemStack>()
+                {
+                    @Override
+                    public int compare(ItemStack stack0, ItemStack stack1)
+                    {
+                        return stack1.getItemDamage() - stack0.getItemDamage();
+                    }
+                });
+                return getItemStackDisplayName(dropped.get(0));
+            }
+
+            return getBlockDisplayName(block, world.getBlockMetadata(x, y, z));
+        }
+
+        return nullBlockString;
     }
 
     public static String getTileEntityDisplayName(TileEntity tile)
     {
-        String displayName = "<NULL>";
-
         if (tile != null)
         {
-            ItemStack itemstack = new ItemStack(tile.getBlockType(), 0, tile.getBlockMetadata());
-            displayName = itemstack.getDisplayName();
+            return getBlockDisplayName(tile.worldObj, tile.xCoord, tile.yCoord, tile.zCoord);
         }
 
-        return displayName;
+        return nullBlockString;
+    }
+
+    public static String getBlockDisplayName(Block block, int blockMeta)
+    {
+        if (block != null)
+        {
+            ItemStack itemstack = new ItemStack(block, 1, blockMeta);
+            return getItemStackDisplayName(itemstack);
+        }
+
+        return nullBlockString;
+    }
+
+    public static String getItemStackDisplayName(ItemStack itemstack)
+    {
+        if (itemstack != null && itemstack.getItem() != null)
+        {
+            return itemstack.getDisplayName();
+        }
+
+        return nullBlockString;
     }
 }
