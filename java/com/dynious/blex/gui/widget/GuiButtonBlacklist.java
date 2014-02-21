97894e99ee5c84f8442e59a598068faf69ce606f
Dynious <iDynious@gmail.com>
2014-02-21 15:38:57 +0100

    Initializing localization support

--- java/com/dynious/blex/gui/widget/GuiButtonBlacklist.java
+++ java/com/dynious/blex/gui/widget/GuiButtonBlacklist.java
@@ -1,10 +1,12 @@
 package com.dynious.blex.gui.widget;
 
 import com.dynious.blex.gui.IGuiParent;
+import com.dynious.blex.lib.Strings;
 import com.dynious.blex.network.PacketTypeHandler;
 import com.dynious.blex.network.packet.PacketBlacklist;
 import com.dynious.blex.tileentity.IFilterTile;
 import cpw.mods.fml.common.network.PacketDispatcher;
+import net.minecraft.util.StatCollector;
 
 import java.util.List;
 
@@ -34,7 +36,7 @@ public class GuiButtonBlacklist extends GuiButtonToggle
     {
         List<String> tooltip = super.getTooltip(mouseX, mouseY);
         if (isMouseInsideBounds(mouseX, mouseY))
-            tooltip.add(getState() ? "Blacklist" : "Whitelist");
+            tooltip.add(StatCollector.translateToLocal(getState() ? Strings.BLACKLIST : Strings.WHITELIST));
         return tooltip;
     }
 
---
+++ java/com/dynious/blex/lib/Strings.java
@@ -0,0 +1,9 @@
+package com.dynious.blex.lib;
+
+public class Strings
+{
+    private static final String GUI_PREFIX = "gui." + Reference.modid.toLowerCase() + ".";
+
+    public static final String BLACKLIST = GUI_PREFIX + "blacklist";
+    public static final String WHITELIST = GUI_PREFIX + "whitelist";
+}
