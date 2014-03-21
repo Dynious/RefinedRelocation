33657d283d12041f37a80d9dcefdfbc83d1146aa
Dynious <iDynious@gmail.com>
2014-03-21 14:20:15 +0100

    Added upgrade support for barrel

--- java/com/dynious/refinedrelocation/mods/JabbaHelper.java
+++ java/com/dynious/refinedrelocation/mods/JabbaHelper.java
@@ -4,10 +4,17 @@ import com.dynious.refinedrelocation.block.BlockSortingBarrel;
 import com.dynious.refinedrelocation.block.ModBlocks;
 import com.dynious.refinedrelocation.lib.BlockIds;
 import com.dynious.refinedrelocation.lib.Names;
+import com.dynious.refinedrelocation.tileentity.TileSortingBarrel;
+import cpw.mods.fml.common.ObfuscationReflectionHelper;
 import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.ironchest.TileEntityIronChest;
 import mcp.mobius.betterbarrels.BetterBarrels;
+import mcp.mobius.betterbarrels.common.blocks.TileEntityBarrel;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
 
 public class JabbaHelper
 {
@@ -20,5 +27,28 @@ public class JabbaHelper
     public static void addBarrelRecipes()
     {
         GameRegistry.addShapedRecipe(new ItemStack(ModBlocks.sortingBarrel), "g g", " b ", "g g", 'g', Item.ingotGold, 'b', new ItemStack(BetterBarrels.blockBarrel));
+    }
+
+    public static boolean upgradeIronToFilteringChest(TileEntity tile)
+    {
+        if (tile instanceof TileEntityBarrel && !(tile instanceof TileSortingBarrel))
+        {
+            World world = tile.getWorldObj();
+
+            TileEntityBarrel oldBarrel = (TileEntityBarrel) tile;
+            int meta = oldBarrel.getBlockMetadata();
+            TileSortingBarrel newBarrel = new TileSortingBarrel();
+            NBTTagCompound tag = new NBTTagCompound();
+            oldBarrel.writeToNBT(tag);
+            newBarrel.readFromNBT(tag);
+            // Clear the old block out
+            world.setBlockToAir(tile.xCoord, tile.yCoord, tile.zCoord);
+            // And put in our block instead
+            world.setBlock(tile.xCoord, tile.yCoord, tile.zCoord, ModBlocks.sortingBarrel.blockID, meta, 3);
+
+            world.setBlockTileEntity(tile.xCoord, tile.yCoord, tile.zCoord, newBarrel);
+            return true;
+        }
+        return false;
     }
 }
