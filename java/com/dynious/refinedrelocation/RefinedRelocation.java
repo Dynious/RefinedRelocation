8e73afce3a1b778226211b9ce3dd297e472451f9
Dynious <iDynious@gmail.com>
2014-03-25 22:12:33 +0100

    Fixed Logger

--- java/com/dynious/refinedrelocation/RefinedRelocation.java
+++ java/com/dynious/refinedrelocation/RefinedRelocation.java
@@ -31,8 +31,6 @@ public class RefinedRelocation
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-        LogHelper.init();
-        
         VersionChecker.execute();
 
         ConfigHandler.init(event.getSuggestedConfigurationFile());
