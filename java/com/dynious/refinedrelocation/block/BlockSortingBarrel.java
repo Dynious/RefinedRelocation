61467aa702054b7da8374bc116a9f3f7608a98b1
Dynious <iDynious@gmail.com>
2014-03-27 17:06:36 +0100

    Temp fix JABBA icon problem (will be fixed in next JABBA version)

--- java/com/dynious/refinedrelocation/block/BlockSortingBarrel.java
+++ java/com/dynious/refinedrelocation/block/BlockSortingBarrel.java
@@ -4,6 +4,7 @@ import com.dynious.refinedrelocation.RefinedRelocation;
 import com.dynious.refinedrelocation.lib.Names;
 import com.dynious.refinedrelocation.tileentity.TileSortingBarrel;
 import mcp.mobius.betterbarrels.common.blocks.BlockBarrel;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
@@ -46,8 +47,7 @@ public class BlockSortingBarrel extends BlockBarrel
     }
 
     @Override
-    public Icon getBlockTexture(IBlockAccess par1IBlockAccess, int x, int y, int z, int side)
+    public void registerIcons(IconRegister iconRegister)
     {
-        return super.getBlockTexture(par1IBlockAccess, x, y, z, side);
     }
 }
