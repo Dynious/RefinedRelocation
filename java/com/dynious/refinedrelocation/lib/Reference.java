d04e995a34d79f3947437118cede540eebd83ff0
Dynious <iDynious@gmail.com>
2014-03-29 20:48:01 +0100

    Bump version number

--- java/com/dynious/refinedrelocation/lib/Reference.java
+++ java/com/dynious/refinedrelocation/lib/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
     public static final String MOD_ID = "RefinedRelocation";
     public static final String NAME = "Refined Relocation";
-    public static final String VERSION = "1.0.0-beta5";
+    public static final String VERSION = "1.0.0-beta6";
     public static final String CLIENT_PROXY = "com.dynious.refinedrelocation.proxy.ClientProxy";
     public static final String COMMON_PROXY = "com.dynious.refinedrelocation.proxy.CommonProxy";
     public static final String CHANNEL_NAME = "RefRelocation";
