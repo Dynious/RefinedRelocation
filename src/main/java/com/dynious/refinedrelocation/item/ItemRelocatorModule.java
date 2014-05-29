d46f35a8bc2043ee7eb95a2c7c816c62006337c0
Dynious <iDynious@gmail.com>
2014-05-29 17:29:31 +0200

    Last few preperations for the alpha release!

--- src/main/java/com/dynious/refinedrelocation/item/ItemRelocatorModule.java
+++ src/main/java/com/dynious/refinedrelocation/item/ItemRelocatorModule.java
@@ -7,14 +7,20 @@ import com.dynious.refinedrelocation.grid.relocator.RelocatorModuleExtraction;
 import com.dynious.refinedrelocation.grid.relocator.RelocatorModuleFilter;
 import com.dynious.refinedrelocation.grid.relocator.RelocatorModuleOneWay;
 import com.dynious.refinedrelocation.lib.Names;
+import com.dynious.refinedrelocation.lib.Resources;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Icon;
 
 import java.util.List;
 
 public class ItemRelocatorModule extends Item implements IItemRelocatorModule
 {
+    private Icon[] icons = new Icon[4];
+
     public ItemRelocatorModule(int id)
     {
         super(id);
@@ -59,5 +65,25 @@ public class ItemRelocatorModule extends Item implements IItemRelocatorModule
         {
             par3List.add(new ItemStack(par1, 1, j));
         }
+    }
+
+    @Override
+    public void addInformation(ItemStack itemStack, EntityPlayer player, List list, boolean b)
+    {
+    }
+
+    @Override
+    public void registerIcons(IconRegister par1IconRegister)
+    {
+        for (int i = 0; i < icons.length; i++)
+        {
+            icons[i] = par1IconRegister.registerIcon(Resources.MOD_ID + ":" + Names.relocatorModule + i);
+        }
+    }
+
+    @Override
+    public Icon getIconFromDamage(int meta)
+    {
+        return meta < icons.length ? icons[meta] : null;
     }
 }
