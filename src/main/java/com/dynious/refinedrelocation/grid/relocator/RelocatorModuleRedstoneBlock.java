f6740d9167f7671e08e69b32552a32d21547d6ed
MCMicky2506 <minecraft.micky2506@gmail.com>
2014-06-23 17:43:38 -0600

    Changes the redstone block module to be toggleable (on right click) to either allow items through on redstone signal, or block items on redstone signal.
    Still needs work, as the icon doesn't update properly.

--- src/main/java/com/dynious/refinedrelocation/grid/relocator/RelocatorModuleRedstoneBlock.java
+++ src/main/java/com/dynious/refinedrelocation/grid/relocator/RelocatorModuleRedstoneBlock.java
@@ -5,8 +5,12 @@ import com.dynious.refinedrelocation.api.relocator.RelocatorModuleBase;
 import com.dynious.refinedrelocation.tileentity.IRelocator;
 import com.dynious.refinedrelocation.item.ModItems;
 import com.dynious.refinedrelocation.lib.Resources;
+import com.dynious.refinedrelocation.lib.Strings;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ChatMessageComponent;
+import net.minecraft.util.StatCollector;
 import net.minecraft.util.Icon;
 
 import java.util.Arrays;
@@ -16,11 +20,28 @@ public class RelocatorModuleRedstoneBlock extends RelocatorModuleBase
 {
     private static Icon iconOn;
     private static Icon iconOff;
+    private boolean blockOnSignal = true;
 
     @Override
     public boolean passesFilter(IItemRelocator relocator, int side, ItemStack stack, boolean input, boolean simulate)
     {
-        return !relocator.getRedstoneState();
+        return blockOnSignal ? !relocator.getRedstoneState() : relocator.getRedstoneState();
+    }
+
+    @Override
+    public boolean onActivated(IItemRelocator relocator, EntityPlayer player, int side, ItemStack stack)
+    {
+        if (player.worldObj.isRemote)
+        {
+            return true;
+        }
+        else
+        {
+            blockOnSignal = !blockOnSignal;
+            player.sendChatToPlayer(new ChatMessageComponent()
+                .addText(StatCollector.translateToLocal(blockOnSignal ? Strings.REDSTONE_BLOCK_ENABLED : Strings.REDSTONE_BLOCK_DISABLED)));
+            return true;
+        }
     }
 
     @Override
@@ -33,12 +54,13 @@ public class RelocatorModuleRedstoneBlock extends RelocatorModuleBase
     public List<ItemStack> getDrops(IItemRelocator relocator, int side)
     {
         return Arrays.asList(new ItemStack(ModItems.relocatorModule, 1, 7));
+
     }
 
     @Override
     public Icon getIcon(IItemRelocator relocator, int side)
     {
-        return relocator.getRedstoneState() ? iconOn : iconOff;
+        return relocator.getRedstoneState() ? (blockOnSignal ? iconOn : iconOff) : (blockOnSignal ? iconOff : iconOn);
     }
 
     @Override
