49e261839c6ec1288a47b4a696c757a04e0c14d2
MCMicky2506 <minecraft.micky2506@gmail.com>
2014-06-27 07:56:25 -0600

    Add localization for Stuffed: in Relocator WAILA HUD.

--- src/main/java/com/dynious/refinedrelocation/mods/waila/RelocatorHUDHandler.java
+++ src/main/java/com/dynious/refinedrelocation/mods/waila/RelocatorHUDHandler.java
@@ -2,17 +2,19 @@ package com.dynious.refinedrelocation.mods.waila;
 
 import codechicken.multipart.TileMultipart;
 import com.dynious.refinedrelocation.grid.relocator.RelocatorModuleRegistry;
+import com.dynious.refinedrelocation.lib.Strings;
 import com.dynious.refinedrelocation.lib.Mods;
 import com.dynious.refinedrelocation.part.PartRelocator;
 import com.dynious.refinedrelocation.tileentity.IRelocator;
 import com.dynious.refinedrelocation.tileentity.TileRelocator;
 import mcp.mobius.waila.api.*;
-import static mcp.mobius.waila.api.SpecialChars.*; // Staticly import to allow us to use these without SpecialChars.TAB;
+import static mcp.mobius.waila.api.SpecialChars.*; // Staticly import to allow us to use these without SpecialChars.;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.StatCollector;
 import net.minecraftforge.common.ForgeDirection;
 import com.dynious.refinedrelocation.api.relocator.IRelocatorModule;
 
@@ -105,7 +107,7 @@ public class RelocatorHUDHandler implements IWailaDataProvider
                 for (int i = 0; i < stuffedStrings.size(); i++)
                 {
                     String finalString = "";
-                    if (i == 0) finalString += "Stuffed: ";
+                    if (i == 0) finalString += StatCollector.translateToLocal(Strings.RELOCATOR_STUFFED) + " ";
 
                     strings.add(finalString + TAB + ALIGNRIGHT + stuffedStrings.get(i));
                 }
