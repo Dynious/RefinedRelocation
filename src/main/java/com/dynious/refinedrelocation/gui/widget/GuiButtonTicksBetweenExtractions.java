f74129a071c7f97ff0f0bedbd098ad933dd22d0e
MCMicky2506 <minecraft.micky2506@gmail.com>
2014-06-27 18:23:37 -0600

    Add decimal formatting so that the seconds get trailing zeros removed.

--- src/main/java/com/dynious/refinedrelocation/gui/widget/GuiButtonTicksBetweenExtractions.java
+++ src/main/java/com/dynious/refinedrelocation/gui/widget/GuiButtonTicksBetweenExtractions.java
@@ -11,9 +11,11 @@ import net.minecraft.util.StatCollector;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.text.DecimalFormat;
 
 public class GuiButtonTicksBetweenExtractions extends GuiButtonCounter
 {
+    private static final DecimalFormat decimalFormat = new DecimalFormat("##.##");
     private RelocatorModuleExtraction module;
 
     public GuiButtonTicksBetweenExtractions(IGuiParent parent, int x, int y, RelocatorModuleExtraction module)
@@ -40,9 +42,10 @@ public class GuiButtonTicksBetweenExtractions extends GuiButtonCounter
         if (isMouseInsideBounds(mouseX, mouseY))
         {
             float seconds = (float) module.getTicksBetweenExtraction() / 20;
+
             List<String> tooltip = new ArrayList<String>();
             tooltip.add(StatCollector.translateToLocal(Strings.TICKS_BETWEEN_EXT));
-            tooltip.add(seconds + " " + StatCollector.translateToLocal(Strings.SECONDS_BETWEEN_EXT));
+            tooltip.add(decimalFormat.format(seconds) + " " + StatCollector.translateToLocal(Strings.SECONDS_BETWEEN_EXT));
             tooltip.addAll(subTooltip);
             return tooltip;
         }
