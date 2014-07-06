5257b24096b56b5182574b5914b0c01a9a1eb672
MCMicky2506 <minecraft.micky2506@gmail.com>
2014-07-06 05:28:54 -0600

    Continue extending relocator controller rendering.

--- src/main/java/com/dynious/refinedrelocation/renderer/RendererMultiBlock.java
+++ src/main/java/com/dynious/refinedrelocation/renderer/RendererMultiBlock.java
@@ -6,6 +6,7 @@ import com.dynious.refinedrelocation.multiblock.TileMultiBlockBase;
 import com.dynious.refinedrelocation.util.BlockAndMeta;
 import com.dynious.refinedrelocation.util.MultiBlockAndMeta;
 import com.dynious.refinedrelocation.util.Vector3;
+import com.sun.org.apache.bcel.internal.generic.IMUL;
 import cpw.mods.fml.common.ObfuscationReflectionHelper;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
@@ -14,7 +15,9 @@ import net.minecraft.client.renderer.RenderGlobal;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.common.util.ForgeDirection;
 import org.lwjgl.opengl.GL11;
 
 public class RendererMultiBlock extends TileEntitySpecialRenderer
@@ -45,27 +48,11 @@ public class RendererMultiBlock extends TileEntitySpecialRenderer
                         {
                             for (int z = 0; z < multiBlock.getMultiBlockMap().getSizeZ(); z++)
                             {
-                                if (!tileMultiBlock.getWorldObj().isAirBlock(tileMultiBlock.xCoord + x - leaderPos.getX(), tileMultiBlock.yCoord + y - leaderPos.getY(), tileMultiBlock.zCoord + z - leaderPos.getZ()))
-                                {
-                                    Block block = tileMultiBlock.getWorldObj().getBlock(tileMultiBlock.xCoord + x - leaderPos.getX(), tileMultiBlock.yCoord + y - leaderPos.getY(), tileMultiBlock.zCoord + z - leaderPos.getZ());
-                                    if (block != null && block.getRenderType() == 0)
-                                        continue;
-                                }
                                 Object blockInfo = multiBlock.getMultiBlockMap().getBlockAndMetaAtPos(x, y, z);
 
                                 GL11.glPushMatrix();
-
-                                if (blockInfo instanceof MultiBlockAndMeta)
-                                {
-                                    MultiBlockAndMeta multiBlockAndMeta = (MultiBlockAndMeta) blockInfo;
-                                    int timePerBlock = 20 / multiBlockAndMeta.getBlockAndMetas().size();
-                                    int blockPlace = (tileMultiBlock.timer % 20) / timePerBlock;
-                                    renderBlock(multiBlockAndMeta.getBlockAndMetas().get(blockPlace), x - leaderPos.getX(), y - leaderPos.getY(), z - leaderPos.getZ());
-                                }
-                                else if (blockInfo instanceof BlockAndMeta)
-                                {
-                                    renderBlock((BlockAndMeta) blockInfo, x - leaderPos.getX(), y - leaderPos.getY(), z - leaderPos.getZ());
-                                }
+                                
+                                renderBlocks(multiBlock, tileMultiBlock, x, y, z);
 
                                 GL11.glPopMatrix();
                             }
@@ -77,15 +64,84 @@ public class RendererMultiBlock extends TileEntitySpecialRenderer
         }
     }
 
-    private void renderBlock(BlockAndMeta blockAndMeta, int relativeX, int relativeY, int relativeZ)
+    private void renderBlocks(IMultiBlock multiBlock, TileMultiBlockBase tileMultiBlock, int x, int y, int z)
     {
-        if (blockAndMeta.getBlock() != null)
+        Vector3 leaderPos = multiBlock.getRelativeLeaderPos();
+        Object blockInfo = multiBlock.getMultiBlockMap().getBlockAndMetaAtPos(x, y, z);
+        int xOffset = tileMultiBlock.xCoord + x - leaderPos.getX();
+        int yOffset = tileMultiBlock.yCoord + y - leaderPos.getY();
+        int zOffset = tileMultiBlock.zCoord + z - leaderPos.getZ();
+
+        if (!tileMultiBlock.getWorldObj().isAirBlock(tileMultiBlock.xCoord + x - leaderPos.getX(), tileMultiBlock.yCoord + y - leaderPos.getY(), tileMultiBlock.zCoord + z - leaderPos.getZ()))
+        {
+            Block block = tileMultiBlock.getWorldObj().getBlock(tileMultiBlock.xCoord + x - leaderPos.getX(), tileMultiBlock.yCoord + y - leaderPos.getY(), tileMultiBlock.zCoord + z - leaderPos.getZ());
+            BlockAndMeta blockAndMeta = null;
+            if (blockInfo instanceof MultiBlockAndMeta)
+            {
+                blockAndMeta = getBlockAndMeta((MultiBlockAndMeta) blockInfo, tileMultiBlock);
+            }
+            else if (blockInfo instanceof BlockAndMeta)
+            {
+                blockAndMeta = (BlockAndMeta) blockInfo;
+            }
+
+            if (blockAndMeta != null && blockAndMeta.getBlock() == block && blockAndMeta.getMeta() == tileMultiBlock.getWorldObj().getBlockMetadata(xOffset, yOffset, zOffset))
+            {
+                // No need for any thing, we have the right block at these coordinates
+            }
+            else
+            {
+                renderIncorrectBlock(x, y, z, xOffset, yOffset, zOffset);
+            }
+        }
+        else
+        {
+            renderBlock(blockInfo, multiBlock, tileMultiBlock, x, y, z);
+        }
+    }
+
+    private void renderBlock(Object blockInfo, IMultiBlock multiBlock, TileMultiBlockBase tileMultiBlock, int x, int y, int z)
+    {
+        Vector3 leaderPos = multiBlock.getRelativeLeaderPos();
+        BlockAndMeta blockAndMeta = null;
+        int relativeX = 0;
+        int relativeY = 0;
+        int relativeZ = 0;
+
+        if (blockInfo instanceof MultiBlockAndMeta)
+        {
+            MultiBlockAndMeta multiBlockAndMeta = (MultiBlockAndMeta) blockInfo;
+
+            blockAndMeta = getBlockAndMeta(multiBlockAndMeta, tileMultiBlock);
+            relativeX = x - leaderPos.getX();
+            relativeY = y - leaderPos.getY();
+            relativeZ = z - leaderPos.getZ();
+        }
+        else if (blockInfo instanceof BlockAndMeta)
+        {
+            blockAndMeta = (BlockAndMeta) blockInfo;
+            relativeX = x - leaderPos.getX();
+            relativeY = y - leaderPos.getY();
+            relativeZ = z - leaderPos.getZ();
+        }
+
+        if (blockAndMeta != null && blockAndMeta.getBlock() != null)
         {
             GL11.glTranslatef(relativeX, relativeY, relativeZ);
             float scale = 0.5F;
             GL11.glScalef(scale, scale, scale);
             renderBlocks.renderBlockAsItem(blockAndMeta.getBlock(), blockAndMeta.getMeta(), 255F);
-            //Minecraft.getMinecraft().renderGlobal.globalRenderBlocks.renderBlockByRenderType(Block.blocksList[blockAndMeta.getBlockId()], x - leaderPos.getX(), y - leaderPos.getY(), z - leaderPos.getZ());
         }
+    }
+
+    private void renderIncorrectBlock(int x, int y, int z, int xOffset, int yOffset, int zOffset)
+    {
+        //TODO: Render incorrect block
+    }
+
+    private BlockAndMeta getBlockAndMeta(MultiBlockAndMeta multiBlockAndMeta, TileMultiBlockBase tileMultiBlock)
+    {
+        int timePerBlock = 20 / multiBlockAndMeta.getBlockAndMetas().size();
+        return multiBlockAndMeta.getBlockAndMetas().get((tileMultiBlock.timer % 20) / timePerBlock);
     }
 }
