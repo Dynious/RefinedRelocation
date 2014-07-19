c56df43d82584cc7d90adb5fd2e51c6bc26bbc57
Micky2506 <minecraft.micky2506@gmail.com>
2014-07-19 09:28:19 -0600

    Add shapeless crafting recipe for Sneaky Extraction Module.

--- src/main/java/com/dynious/refinedrelocation/item/ModItems.java
+++ src/main/java/com/dynious/refinedrelocation/item/ModItems.java
@@ -57,6 +57,7 @@ public class ModItems
         GameRegistry.addShapedRecipe(new ItemStack(relocatorModule, 1, 8), "g g", " r ", "g g", 'g', Items.glowstone_dust, 'r', new ItemStack(relocatorModule, 1, 0));
         GameRegistry.addShapedRecipe(new ItemStack(relocatorModule, 1, 9), "d d", " r ", "t t", 'd', Items.redstone, 't', Blocks.redstone_torch, 'r', new ItemStack(relocatorModule, 1, 0));
         GameRegistry.addShapedRecipe(new ItemStack(relocatorModule, 1, 10), "d d", " r ", "g g", 'd', Items.diamond, 'g', Items.gold_ingot, 'r', new ItemStack(relocatorModule, 1, 0));
+        GameRegistry.addShapelessRecipe(new ItemStack(relocatorModule, 1, 11), new ItemStack(relocatorModule, 1, 5), new ItemStack(relocatorModule, 1, 3));
 
         GameRegistry.addShapedRecipe(new ItemStack(toolBox), "isi", "i i", "iii", 'i', Items.iron_ingot, 's', Items.stick);
         GameRegistry.addRecipe(new RecipeToolbox());
