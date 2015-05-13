41df6eff6091f8dc5b05363ceda2d96be23525a2
Blay09 <blay09@hotmail.de>
2015-05-13 12:56:27 +0200

    Make GUI packets work both ways IF they were registered in the NetworkHandler (but they aren't  since they don't need to be yet).

--- src/main/java/com/dynious/refinedrelocation/network/packet/gui/MessageGUIAction.java
+++ src/main/java/com/dynious/refinedrelocation/network/packet/gui/MessageGUIAction.java
@@ -1,10 +1,12 @@
 package com.dynious.refinedrelocation.network.packet.gui;
 
 import com.dynious.refinedrelocation.container.ContainerRefinedRelocation;
+import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.common.network.ByteBufUtils;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
+import cpw.mods.fml.relauncher.Side;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
@@ -19,7 +21,7 @@ public class MessageGUIAction extends MessageGUI implements IMessageHandler<Mess
 
     @Override
     public IMessage onMessage(MessageGUIAction message, MessageContext ctx) {
-        EntityPlayer entityPlayer = ctx.getServerHandler().playerEntity;
+        EntityPlayer entityPlayer = ctx.side == Side.SERVER ? ctx.getServerHandler().playerEntity : FMLClientHandler.instance().getClientPlayerEntity();
         Container container = entityPlayer.openContainer;
         if(container == null || !(container instanceof ContainerRefinedRelocation)) {
             return null;
--- src/main/java/com/dynious/refinedrelocation/network/packet/gui/MessageGUIByte.java
+++ src/main/java/com/dynious/refinedrelocation/network/packet/gui/MessageGUIByte.java
@@ -1,9 +1,11 @@
 package com.dynious.refinedrelocation.network.packet.gui;
 
 import com.dynious.refinedrelocation.container.ContainerRefinedRelocation;
+import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
+import cpw.mods.fml.relauncher.Side;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
@@ -34,7 +36,7 @@ public class MessageGUIByte extends MessageGUI implements IMessageHandler<Messag
 
     @Override
     public IMessage onMessage(MessageGUIByte message, MessageContext ctx) {
-        EntityPlayer entityPlayer = ctx.getServerHandler().playerEntity;
+        EntityPlayer entityPlayer = ctx.side == Side.SERVER ? ctx.getServerHandler().playerEntity : FMLClientHandler.instance().getClientPlayerEntity();
         Container container = entityPlayer.openContainer;
         if(container == null || !(container instanceof ContainerRefinedRelocation)) {
             return null;
