52f77c21209210f644a184d1e7b0952911f85b5d
Blay09 <blay09@hotmail.de>
2015-05-15 22:18:25 +0200

    Committing to switch branch...

---
+++ src/main/java/com/dynious/refinedrelocation/client/gui/widget/GuiEmptyFilter.java
@@ -0,0 +1,13 @@
+package com.dynious.refinedrelocation.client.gui.widget;
+
+import com.dynious.refinedrelocation.client.gui.IGuiParent;
+
+public class GuiEmptyFilter extends GuiWidgetBase {
+
+    public GuiEmptyFilter(IGuiParent parent, int x, int y, int w, int h) {
+        super(parent, x, y, w, h);
+    }
+
+
+
+}
