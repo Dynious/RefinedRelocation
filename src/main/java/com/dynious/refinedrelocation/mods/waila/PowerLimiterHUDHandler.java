94dc1aed830dde5ec23983b73dbc71e227077736
Dynious <iDynious@gmail.com>
2015-05-17 14:30:14 +0200

    Fixed #293 and updated to WAILA 1.5.10

--- src/main/java/com/dynious/refinedrelocation/mods/waila/PowerLimiterHUDHandler.java
+++ src/main/java/com/dynious/refinedrelocation/mods/waila/PowerLimiterHUDHandler.java
@@ -6,9 +6,12 @@ import com.dynious.refinedrelocation.tileentity.TilePowerLimiter;
 import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaDataAccessor;
 import mcp.mobius.waila.api.IWailaDataProvider;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.StatCollector;
+import net.minecraft.world.World;
 
 import java.util.List;
 
@@ -46,5 +49,13 @@ public class PowerLimiterHUDHandler implements IWailaDataProvider
     public List<String> getWailaTail(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config)
     {
         return null;
+    }
+
+    @Override
+    public NBTTagCompound getNBTData(EntityPlayerMP entityPlayerMP, TileEntity tileEntity, NBTTagCompound nbtTagCompound, World world, int i, int i1, int i2)
+    {
+        if (tileEntity != null)
+            tileEntity.writeToNBT(nbtTagCompound);
+        return nbtTagCompound;
     }
 }
--- src/main/java/com/dynious/refinedrelocation/mods/waila/RelocationControllerHUDHandler.java
+++ src/main/java/com/dynious/refinedrelocation/mods/waila/RelocationControllerHUDHandler.java
@@ -6,9 +6,12 @@ import com.dynious.refinedrelocation.multiblock.TileMultiBlockBase;
 import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaDataAccessor;
 import mcp.mobius.waila.api.IWailaDataProvider;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.StatCollector;
+import net.minecraft.world.World;
 
 import java.util.List;
 
@@ -80,5 +83,13 @@ public class RelocationControllerHUDHandler implements IWailaDataProvider
     public List<String> getWailaTail(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config)
     {
         return null;
+    }
+
+    @Override
+    public NBTTagCompound getNBTData(EntityPlayerMP entityPlayerMP, TileEntity tileEntity, NBTTagCompound nbtTagCompound, World world, int i, int i1, int i2)
+    {
+        if (tileEntity != null)
+            tileEntity.writeToNBT(nbtTagCompound);
+        return nbtTagCompound;
     }
 }
