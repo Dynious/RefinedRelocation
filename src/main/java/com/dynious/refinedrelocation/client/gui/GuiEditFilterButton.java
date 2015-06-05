ec3b2603a4ca12eabd0172050877034c62cfd1ff
Blay09 <blay09@hotmail.de>
2015-06-06 01:24:43 +0200

    Add edit filter button to the side of sorting containers.
    #286

---
+++ src/main/java/com/dynious/refinedrelocation/client/gui/GuiEditFilterButton.java
@@ -0,0 +1,21 @@
+package com.dynious.refinedrelocation.client.gui;
+
+import com.dynious.refinedrelocation.lib.Resources;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+
+public class GuiEditFilterButton extends GuiButton {
+
+	public GuiEditFilterButton(int x, int y) {
+		super(-1, x, y, 31, 26, "");
+	}
+
+	@Override
+	public void drawButton(Minecraft mc, int mouseX, int mouseY) {
+		mc.getTextureManager().bindTexture(Resources.GUI_SHARED);
+		boolean isInside = mouseX >= xPosition && mouseY >= yPosition && mouseX < xPosition + width && mouseY < yPosition + height;
+		drawTexturedModalRect(xPosition, yPosition, isInside ? 0 : 31, 204, width, height);
+		drawTexturedModalRect(xPosition + width / 2 - 7, yPosition + height / 2 - 9, 152, 238, 18, 18);
+	}
+
+}
--- src/main/java/com/dynious/refinedrelocation/event/EventHandlerClient.java
+++ src/main/java/com/dynious/refinedrelocation/event/EventHandlerClient.java
@@ -1,19 +1,27 @@
 package com.dynious.refinedrelocation.event;
 
 import codechicken.lib.raytracer.RayTracer;
+import com.dynious.refinedrelocation.api.APIUtils;
+import com.dynious.refinedrelocation.api.tileentity.IFilterTileGUI;
 import com.dynious.refinedrelocation.api.tileentity.ISortingMember;
 import com.dynious.refinedrelocation.api.tileentity.handlers.IGridMemberHandler;
 import com.dynious.refinedrelocation.block.ModBlocks;
+import com.dynious.refinedrelocation.client.gui.GuiEditFilterButton;
+import com.dynious.refinedrelocation.client.gui.GuiFiltered;
 import com.dynious.refinedrelocation.client.renderer.RendererRelocator;
 import com.dynious.refinedrelocation.container.IContainerFiltered;
 import com.dynious.refinedrelocation.grid.relocator.RelocatorModuleRegistry;
 import com.dynious.refinedrelocation.item.ModItems;
 import com.dynious.refinedrelocation.lib.Strings;
+import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.gui.inventory.GuiContainerCreative;
+import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.RenderGlobal;
 import net.minecraft.client.resources.I18n;
@@ -23,16 +31,21 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.StatCollector;
-import net.minecraftforge.client.event.DrawBlockHighlightEvent;
-import net.minecraftforge.client.event.FOVUpdateEvent;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
-import net.minecraftforge.client.event.TextureStitchEvent;
+import net.minecraft.world.World;
+import net.minecraftforge.client.event.*;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.GL11;
 
 public class EventHandlerClient
 {
+    private World interactionWorld;
+    private int interactionX;
+    private int interactionY;
+    private int interactionZ;
+    private GuiButton editFilterButton;
+
     @SubscribeEvent
     public void FOVEvent(FOVUpdateEvent event)
     {
@@ -132,4 +145,42 @@ public class EventHandlerClient
         block.setBlockBoundsBasedOnState(tile.getWorldObj(), tile.xCoord, tile.yCoord, tile.zCoord);
         RenderGlobal.drawOutlinedBoundingBox(block.getSelectedBoundingBoxFromPool(tile.getWorldObj(), tile.xCoord, tile.yCoord, tile.zCoord).expand(expansion, expansion, expansion).getOffsetBoundingBox(-offsetX, -offsetY, -offsetZ), -1);
     }
+
+    @SubscribeEvent
+    public void interactWith(PlayerInteractEvent event) {
+        // Sadly the only solution I could think of for checking if a GuiContainer belongs to an IFilterTileGUI
+        if(event.action == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK) {
+            interactionWorld = event.world;
+            interactionX = event.x;
+            interactionY = event.y;
+            interactionZ = event.z;
+            return;
+        }
+        interactionWorld = null;
+    }
+
+    @SubscribeEvent
+    public void initGui(GuiScreenEvent.InitGuiEvent event) {
+        if(interactionWorld != null) {
+            TileEntity tileEntity = interactionWorld.getTileEntity(interactionX, interactionY, interactionZ);
+            if(tileEntity instanceof IFilterTileGUI && event.gui instanceof GuiContainer) {
+                // Make sure it's not already a filter GUI or an inventory GUI
+                if (!(event.gui instanceof GuiFiltered) && !(event.gui instanceof GuiInventory) && !(event.gui instanceof GuiContainerCreative)) {
+                    GuiContainer guiContainer = (GuiContainer) event.gui;
+                    if (interactionWorld != null) {
+                        editFilterButton = new GuiEditFilterButton(guiContainer.guiLeft - 31, guiContainer.guiTop + 25);
+                        event.buttonList.add(editFilterButton);
+                    }
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public void actionPerformed(GuiScreenEvent.ActionPerformedEvent event) {
+        if(event.button == editFilterButton) {
+            APIUtils.openFilteringGUI(FMLClientHandler.instance().getClientPlayerEntity(), interactionWorld, interactionX, interactionY, interactionZ);
+        }
+    }
+
 }
