f381067b7d2a1d5c3ca605ea122b22c0a336e8c0
Dynious <iDynious@gmail.com>
2015-08-22 21:35:13 +0200

    Add Relocator tooltip

--- src/main/java/com/dynious/refinedrelocation/mods/part/ItemPartRelocator.java
+++ src/main/java/com/dynious/refinedrelocation/mods/part/ItemPartRelocator.java
@@ -6,9 +6,14 @@ import codechicken.multipart.JItemMultiPart;
 import codechicken.multipart.TMultiPart;
 import com.dynious.refinedrelocation.RefinedRelocation;
 import com.dynious.refinedrelocation.lib.Names;
+import com.dynious.refinedrelocation.lib.Strings;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
+import org.lwjgl.input.Keyboard;
+
+import java.util.List;
 
 public class ItemPartRelocator extends JItemMultiPart
 {
@@ -23,5 +28,22 @@ public class ItemPartRelocator extends JItemMultiPart
     public TMultiPart newPart(ItemStack itemStack, EntityPlayer player, World world, BlockCoord blockCoord, int i, Vector3 vector3)
     {
         return PartFactory.INSTANCE.createPart("tile." + Names.relocator, false);
+    }
+
+    @Override
+    public void addInformation(ItemStack itemStack, EntityPlayer player, List list, boolean flag)
+    {
+        super.addInformation(itemStack, player, list, flag);
+
+        if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT))
+        {
+            String[] tooltipLines = StatCollector.translateToLocal(getUnlocalizedName(itemStack) + ".tooltip").split("\\\\n");
+            for (String s : tooltipLines)
+            {
+                list.add("\u00a73" + s);
+            }
+        } else {
+            list.add("\u00a76" + StatCollector.translateToLocal(Strings.TOOLTIP_SHIFT));
+        }
     }
 }
