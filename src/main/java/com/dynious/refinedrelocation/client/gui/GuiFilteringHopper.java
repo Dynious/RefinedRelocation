5927e0ffd92722869666e247d76886963d34036f
Blay09 <blay09@hotmail.de>
2015-10-08 05:54:03 +0200

    Fix edit filter button missing from Filtering Hopper
    #362

--- src/main/java/com/dynious/refinedrelocation/client/gui/GuiFilteringHopper.java
+++ src/main/java/com/dynious/refinedrelocation/client/gui/GuiFilteringHopper.java
@@ -13,32 +13,34 @@ import net.minecraft.inventory.IInventory;
 import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
 
-public class GuiFilteringHopper extends GuiContainer
-{
+public class GuiFilteringHopper extends GuiContainer {
+
     private static final ResourceLocation hopperGuiTextures = new ResourceLocation("textures/gui/container/hopper.png");
 
     private final TileFilteringHopper filteringHopper;
     private final IInventory playerInventory;
-    private final GuiEditFilterButton editFilterButton;
+    private GuiEditFilterButton editFilterButton;
 
-    public GuiFilteringHopper(InventoryPlayer par1InventoryPlayer, TileFilteringHopper filteringHopper)
-    {
+    public GuiFilteringHopper(InventoryPlayer par1InventoryPlayer, TileFilteringHopper filteringHopper) {
         super(new ContainerFilteringHopper(par1InventoryPlayer, filteringHopper));
         this.playerInventory = par1InventoryPlayer;
         this.filteringHopper = filteringHopper;
         this.allowUserInput = false;
         this.ySize = 133;
+    }
+
+    @Override
+    public void initGui() {
+        super.initGui();
 
         editFilterButton = new GuiEditFilterButton(guiLeft - GuiEditFilterButton.WIDTH, guiTop - GuiEditFilterButton.HEIGHT);
         buttonList.add(editFilterButton);
     }
 
     @Override
-    protected void actionPerformed(GuiButton button)
-    {
+    protected void actionPerformed(GuiButton button) {
         super.actionPerformed(button);
-        if (button == editFilterButton)
-        {
+        if (button == editFilterButton) {
             NetworkHandler.INSTANCE.sendToServer(new MessageOpenFilterGUI(filteringHopper.xCoord, filteringHopper.yCoord, filteringHopper.zCoord));
         }
     }
@@ -47,22 +49,21 @@ public class GuiFilteringHopper extends GuiContainer
      * Draw the foreground layer for the GuiContainer (everything in front of the items)
      */
     @Override
-    protected void drawGuiContainerForegroundLayer(int par1, int par2)
-    {
-        this.fontRendererObj.drawString(this.filteringHopper.hasCustomInventoryName() ? this.filteringHopper.getInventoryName() : I18n.format(this.filteringHopper.getInventoryName()), 8, 6, 4210752);
-        this.fontRendererObj.drawString(this.playerInventory.hasCustomInventoryName() ? this.playerInventory.getInventoryName() : I18n.format(this.playerInventory.getInventoryName()), 8, this.ySize - 96 + 2, 4210752);
+    protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
+        fontRendererObj.drawString(filteringHopper.hasCustomInventoryName() ? filteringHopper.getInventoryName() : I18n.format(filteringHopper.getInventoryName()), 8, 6, 4210752);
+        fontRendererObj.drawString(playerInventory.hasCustomInventoryName() ? playerInventory.getInventoryName() : I18n.format(playerInventory.getInventoryName()), 8, ySize - 96 + 2, 4210752);
     }
 
     /**
      * Draw the background layer for the GuiContainer (everything behind the items)
      */
     @Override
-    protected void drawGuiContainerBackgroundLayer(float par1, int par2, int par3)
-    {
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        this.mc.getTextureManager().bindTexture(hopperGuiTextures);
-        int k = (this.width - this.xSize) / 2;
-        int l = (this.height - this.ySize) / 2;
-        this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.ySize);
+    protected void drawGuiContainerBackgroundLayer(float delta, int mouseX, int mouseY) {
+        GL11.glColor4f(1f, 1f, 1f, 1f);
+        mc.getTextureManager().bindTexture(hopperGuiTextures);
+        int x = (this.width - this.xSize) / 2;
+        int y = (this.height - this.ySize) / 2;
+        this.drawTexturedModalRect(x, y, 0, 0, this.xSize, this.ySize);
     }
+
 }
