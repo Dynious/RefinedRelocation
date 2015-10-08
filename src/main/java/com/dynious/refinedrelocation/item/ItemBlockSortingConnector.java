1758d49b79b90b56581d2e63d7edd839527ae91a
Blay09 <blay09@hotmail.de>
2015-10-08 08:23:12 +0200

    Mark ME Sorting Interface as deprecated.

--- src/main/java/com/dynious/refinedrelocation/item/ItemBlockSortingConnector.java
+++ src/main/java/com/dynious/refinedrelocation/item/ItemBlockSortingConnector.java
@@ -16,53 +16,48 @@ import org.lwjgl.input.Keyboard;
 
 import java.util.List;
 
-public class ItemBlockSortingConnector extends ItemBlock
-{
-    public ItemBlockSortingConnector(Block block)
-    {
+public class ItemBlockSortingConnector extends ItemBlock {
+
+    public ItemBlockSortingConnector(Block block) {
         super(block);
         this.setHasSubtypes(true);
     }
 
     @Override
-    public String getUnlocalizedName(ItemStack stack)
-    {
+    public String getUnlocalizedName(ItemStack stack) {
         return getUnlocalizedName() + stack.getItemDamage();
     }
 
     @Override
-    public int getMetadata(int meta)
-    {
+    public int getMetadata(int meta) {
         return meta;
     }
 
     @Override
-    public boolean placeBlockAt(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ, int metadata)
-    {
-        if (!super.placeBlockAt(stack, player, world, x, y, z, side, hitX, hitY, hitZ, metadata))
-        {
+    public boolean placeBlockAt(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ, int metadata) {
+        if (!super.placeBlockAt(stack, player, world, x, y, z, side, hitX, hitY, hitZ, metadata)) {
             return false;
         }
         TileEntity tile = world.getTileEntity(x, y, z);
-        if (tile != null && tile instanceof TileSortingInterface)
-        {
+        if (tile != null && tile instanceof TileSortingInterface) {
             ((TileSortingInterface) tile).setConnectedSide(ForgeDirection.getOrientation(side).getOpposite());
         }
         return true;
     }
 
     @SideOnly(Side.CLIENT)
-    public void addInformation(ItemStack itemStack, EntityPlayer entityPlayer, List list, boolean flag)
-    {
-        if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT))
-        {
+    public void addInformation(ItemStack itemStack, EntityPlayer entityPlayer, List list, boolean flag) {
+        if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
             String[] tooltipLines = StatCollector.translateToLocal(getUnlocalizedName(itemStack) + ".tooltip").split("\\\\n");
-            for (String s : tooltipLines)
-            {
+            for (String s : tooltipLines) {
                 list.add("\u00a73" + s);
             }
         } else {
             list.add("\u00a76" + StatCollector.translateToLocal(Strings.TOOLTIP_SHIFT));
         }
+        if(itemStack.getItemDamage() == 3) {
+            list.add("\u00a7c" + "[Deprecated] This block will be removed in a future version of Refined Relocation.");
+        }
     }
+
 }
